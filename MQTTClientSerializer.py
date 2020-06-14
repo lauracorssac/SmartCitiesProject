2519d0c3126c1f822b2d3f85aaf405339525d087
Laura <lauracorssac@gmail.com>
2020-06-14 13:38:35 +0200

    changing id to be unique to each client

--- MQTTClientSerializer.py
+++ MQTTClientSerializer.py
@@ -1,12 +1,14 @@
 
 from MQTTClient import MQTTClient
 import json
+from datetime import datetime
 
 CONFIG = "config"
 TOPICS = "topics"
 BROKER_HOST = "brokerHost"
 BROKER_PORT = "brokerPort"
 SUBSCRIBE = "subscribe"
+ID = "id"
 
 class MQTTClientSerializer(object):
 
@@ -15,14 +17,14 @@ class MQTTClientSerializer(object):
         hostname = ""
         port = 0
         topics_to_subscribe = []
-        client_id = "id_client" #todo: change to something better
 
         with open(jsonFile) as f:
-            data_dict = json.load(f)
 
+            data_dict = json.load(f)
             hostname = data_dict[CONFIG][BROKER_HOST]
             port = int(data_dict[CONFIG][BROKER_PORT])
             topics_to_subscribe = data_dict[TOPICS][SUBSCRIBE]
+            client_id = data_dict[CONFIG][ID] + (datetime.utcnow().strftime('%H_%M_%S'))
 
             return MQTTClient(hostname, port, client_id, topics_to_subscribe)
 
